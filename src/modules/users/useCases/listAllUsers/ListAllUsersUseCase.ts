f4177deb152ad005963ee8c3aca8305bacf5d70a
Marcos M. Junior <marcos.mth.jr@gmail.com.br>
2021-05-09 13:18:12 -0300

    Desafio finalizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+    if (!userIsAdmin) {
+      throw new Error("User admin not found");
+    }
+    if (userIsAdmin.admin === false) {
+      throw new Error("you are not allowed to list users");
+    }
+    const allUsers = this.usersRepository.list();
+    return allUsers;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,14 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("User nt found!");
+    }
+    user.admin = true;
+    user.updated_at = new Date();
+
+    return user;
   }
 }
 
